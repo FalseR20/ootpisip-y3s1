1984026490672036af0c6b8b0a60f46dcaa5edf1
FalseR20 <kno3.2002@gmail.com>
2022-12-04 15:37:32 +0300

    style(lab2): restyle

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -2,13 +2,14 @@
 
 #include "texteditor.h"
 
+
 int main(int argc, char *argv[]) {
-  QApplication app(argc, argv);
-  app.setApplicationName("Text Editor");
-  app.setApplicationVersion("0.1");
-  app.setOrganizationName("ExampleSoft");
-  app.setOrganizationDomain("example.com");
-  TextEditor textEditor;
-  textEditor.show();
-  return app.exec();
+    QApplication app(argc, argv);
+    app.setApplicationName("Text Editor");
+    app.setApplicationVersion("0.1");
+    app.setOrganizationName("ExampleSoft");
+    app.setOrganizationDomain("example.com");
+    TextEditor textEditor;
+    textEditor.show();
+    return app.exec();
 }
--- lab2/texteditor.h
+++ lab2/texteditor.h
@@ -4,31 +4,38 @@
 #include <QMainWindow>
 #include <QString>
 
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class TextEditor;
 }
 QT_END_NAMESPACE
 
-class TextEditor : public QMainWindow {
-  Q_OBJECT
+class TextEditor: public QMainWindow {
+Q_OBJECT
 
 public:
-  TextEditor(const QString &fileName = QString(), QWidget *parent = nullptr);
-  ~TextEditor();
+    TextEditor(const QString &fileName = QString(), QWidget *parent = nullptr);
+
+    ~TextEditor();
 
 private slots:
-  bool saveFile();
 
-  bool saveFileAs();
+    bool saveFile();
+
+    bool saveFileAs();
 
 private:
-  void loadFile(const QString &fileName);
-  void setFileName(const QString &fileName);
-  Ui::TextEditor *ui;
-  QString m_fileName;
+    void loadFile(const QString &fileName);
+
+    void setFileName(const QString &fileName);
+
+    Ui::TextEditor *ui;
+    QString m_fileName;
 
 protected:
-  void closeEvent(QCloseEvent *e);
+    void closeEvent(QCloseEvent *e);
 };
+
+
 #endif // TEXTEDITOR_H
